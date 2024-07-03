c874e75b9e4d4c01b71cd7a9e1141b27c57b01d3
Bruno Matheus <brumalones@gmail.com>
2024-07-03 10:10:43 -0300

    first commit - Initial Project

---
+++ src/test/java/br/com/ezblue/ezeurekaserver/EzEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ezblue.ezeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
